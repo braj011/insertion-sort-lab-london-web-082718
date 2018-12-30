631159487f428ae83c1ebe2c777b6a30f01e5b2a
Bhagya Raj <bhagyaraj01@gmail.com>
2018-12-30 17:28:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function findMinAndRemove(array){
   let minimum = array[0]
   index = 0
-  for (let i =0; i < array.length; i++;  )
+  for (let i =0; i < array.length; i++) {
+    if (array[i] < minimum) {
+      minimum = array[i]
+      index = i
+    }
+  }
 
 }
 
