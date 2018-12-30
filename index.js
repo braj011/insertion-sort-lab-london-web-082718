20541b6e9e5e7eba9db4afb89c89dd4065b263b3
Bhagya Raj <bhagyaraj01@gmail.com>
2018-12-30 17:54:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ function findMinAndRemove(array){
 }
 
 function insertionSort(array){
+  let sorted = []
+  while (array.length > 0) {
+    findMinAndRemove(array)
+    sorted.push(minimum)
+  }
 
 }
